11fe0978665f97f18d48fe795261529551a5101c
FaithzaMandolang <s22210337@student.unklab.ac.id>
2024-11-12 23:19:56 +0800

    modif home-about

--- project-akhir/src/App.js
+++ project-akhir/src/App.js
@@ -1,9 +1,11 @@
+import About from "./components/about/about";
 import Home from "./components/home";
 
 const App = () => {
   return (
     <div>
       <Home />
+      <About />
     </div>
   );
 };
