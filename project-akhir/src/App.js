03994870327a62bd50cd49661681cc7d8e7e691f
FAJRIKUN-MAKALALAG <S2210271@student.unklab.ac.id>
2024-11-12 23:24:52 +0800

    adding Testimonial-Service-Reservation

--- project-akhir/src/App.js
+++ project-akhir/src/App.js
@@ -2,6 +2,9 @@ import About from "./components/about/about";
 import Contact from "./components/contact/contact";
 import Home from "./components/home";
 import Menu from "./components/menu/menu";
+import Reservation from "./components/reservation/reservation";
+import Service from "./components/service/service";
+import Testimonial from "./components/testimonial/testimonial";
 
 const App = () => {
   return (
@@ -10,6 +13,9 @@ const App = () => {
       <About />
       <Contact />
       <Menu />
+      <Testimonial />
+      <Service />
+      <Reservation />
     </div>
   );
 };
