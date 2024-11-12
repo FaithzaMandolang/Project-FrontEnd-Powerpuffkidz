cb850db4cc623b24ae36833b5a431245e31c245b
gracealynn <s22210508@student.unklab.ac.id>
2024-11-12 23:22:52 +0800

    adding contact - menu

--- project-akhir/src/App.js
+++ project-akhir/src/App.js
@@ -1,11 +1,15 @@
 import About from "./components/about/about";
+import Contact from "./components/contact/contact";
 import Home from "./components/home";
+import Menu from "./components/menu/menu";
 
 const App = () => {
   return (
     <div>
       <Home />
       <About />
+      <Contact />
+      <Menu />
     </div>
   );
 };
