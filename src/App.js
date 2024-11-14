6ab0adaaf00b0a02feb2db80bbf581e8131e6b39
FaithzaMandolang <s22210337@student.unklab.ac.id>
2024-11-14 22:01:06 +0800

    modify assets-css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-import "./assets/css/bootstrap-icons.css";
-import "./assets/css/bootstrap.min.css";
-import "./assets/css/tooplate-barista.css";
-import "./assets/css/vegas.min.css";
 import Home from "./components";
 import Reservation from "./components/reservasi";
 
