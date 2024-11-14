6a138e2bf51eedd9e074104dd35b864d696c7688
FaithzaMandolang <s22210337@student.unklab.ac.id>
2024-11-14 20:46:00 +0800

    modif app.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./assets/css/bootstrap.min.css";
 import "./assets/css/tooplate-barista.css";
 import "./assets/css/vegas.min.css";
 import Home from "./components";
+import Reservation from "./components/reservasi";
 
 function App() {
   return (
     <div>
       <Home />
+      <Reservation />
     </div>
   );
 }
