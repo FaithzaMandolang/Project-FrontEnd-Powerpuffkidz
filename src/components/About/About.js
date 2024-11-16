77a73965a133ef4fd404d429650f188cf747dc38
Fajrikun <S22210271@student.unklab.ac.id>
2024-11-17 00:50:12 +0800

    adding firebase-About

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,4 +1,20 @@
+import { getDatabase, ref, onValue } from "firebase/database";
+import { useEffect, useState } from "react";
+
 const About = () => {
+  const [About, setAbout] = useState({});
+  useEffect(() => {
+    const db = getDatabase();
+    const AboutRef = ref(db, "About/");
+    onValue(
+      AboutRef,
+      (snapshot) => {
+        const data = snapshot.val();
+        setAbout(data);
+      },
+      []
+    );
+  });
   return (
     <section className="about-section section-padding" id="section_2">
       <div className="section-overlay" />
@@ -11,39 +27,38 @@ const About = () => {
                   src="videos/pexels-mike-jones-9046237.mp4"
                   type="video/mp4"
                 />
-                Your browser does not support the video tag. {/*ini*/}
+                {About.info}
               </video>
               <div className="about-video-info d-flex flex-column">
-                <h4 className="mt-auto">We Started Since 2020.</h4>
-                {/*ini*/}
-                <h4>Best Cafe in Airmadidi.</h4>
-                {/*ini*/}
+                <h4 className="mt-auto">{About.since}</h4>
+
+                <h4>{About.best}</h4>
               </div>
             </div>
           </div>
           <div className="col-lg-5 col-12 mt-4 mt-lg-0 mx-auto">
-            <em className="text-white">Aksara.co</em> {/*ini*/}
-            <h2 className="text-white mb-3">Aksara Cafe</h2>
-            {/*ini*/}
+            <em className="text-white">{About.nama}</em>
+            <h2 className="text-white mb-3">{About.since}</h2>
+
             <p className="text-white">
               we believe that every cup of coffee tells a story. Born from our
               love of flavor and community, we created a space where the aroma
               of fresh coffee and delicious food blends with laughter and
               togetherness. Come in, unwind, and be a part of our story.
             </p>
-            {/*ini bekg list baru. paragraf 1*/}
+
             <p className="text-white">
               We take pride in serving the best from local farmers and carefully
               selected quality ingredients. Every dish is crafted with care,
               creating an experience that delights the palate and warms the
               soul.
             </p>
-            {/*ini. paragraf2*/}
+
             <a
               href="#barista-team"
               className="smoothscroll btn custom-btn custom-border-btn mt-3 mb-4"
             >
-              Meet Baristas {/*ini*/}
+              {About.btn1}
             </a>
           </div>
         </div>
