d8c1a0aa2a3da926b1c8ef3bdcfa62bb87c6bfcf
gracealynn <s22210508@student.unklab.ac.id>
2024-11-17 20:23:54 +0800

    adding menu to db

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,4 +1,34 @@
+import { getDatabase, ref, onValue } from "firebase/database";
+import { useEffect, useState } from "react";
+
 const Menu = () => {
+  const [Breakfast, setBreakfast] = useState({});
+  useEffect(() => {
+    const db = getDatabase();
+    const BreakfastRef = ref(db, "Breakfast/");
+    onValue(
+      BreakfastRef,
+      (snapshot) => {
+        const data = snapshot.val();
+        setBreakfast(data);
+      },
+      []
+    );
+  });
+
+  const [Coffee, setCoffee] = useState({});
+  useEffect(() => {
+    const db = getDatabase();
+    const CoffeeRef = ref(db, "Coffee/");
+    onValue(
+      CoffeeRef,
+      (snapshot) => {
+        const data = snapshot.val();
+        setCoffee(data);
+      },
+      []
+    );
+  });
   return (
     <section className="menu-section section-padding" id="section_3">
       <div className="container">
@@ -7,72 +37,62 @@ const Menu = () => {
             <div className="menu-block-wrap">
               <div className="text-center mb-4 pb-lg-2">
                 <em className="text-white">Delicious Menu</em>
-                <h4 className="text-white">{Menu.Breakfast.nama}</h4>
+                <h4 className="text-white">Breakfast</h4>
               </div>
               <div className="menu-block">
                 <div className="d-flex">
-                  <h6>{Menu.Breakfast.Pancake.nama}</h6>
+                  <h6>{Breakfast.nama1}</h6>
                   <span className="underline" />
-                  <strong className="ms-auto">
-                    {Menu.Breakfast.Pancake.price}
-                  </strong>
+                  <strong className="ms-auto">{Breakfast.price1}</strong>
                 </div>
                 <div className="border-top mt-2 pt-2">
-                  <small>{Menu.Breakfast.Pancake.ket}</small>
+                  <small>Fresh brewed coffee and steamed milk</small>
                 </div>
               </div>
               <div className="menu-block my-4">
                 <div className="d-flex">
-                  <h6>{Menu.Breakfast.ToastedWaffles.nama}</h6>
+                  <h6>{Breakfast.nama2}</h6>
                   <span className="underline" />
                   <strong className="text-white ms-auto">
                     <del>$16.50</del>
                   </strong>
-                  <strong className="ms-2">
-                    {Menu.Breakfast.ToastedWaffles.price}
-                  </strong>
+                  <strong className="ms-2">{Breakfast.price2}</strong>
                 </div>
                 <div className="border-top mt-2 pt-2">
-                  <small>{Menu.Breakfast.ToastedWaffles.ket}</small>
+                  <small>Brewed coffee and steamed milk</small>
                 </div>
               </div>
               <div className="menu-block">
                 <div className="d-flex">
                   <h6>
-                    {Menu.Breakfast.FriedChips.nama}
+                    {Breakfast.nama3}
                     <span className="badge ms-3">Recommend</span>
                   </h6>
                   <span className="underline" />
-                  <strong className="ms-auto">
-                    {Menu.Breakfast.FriedChips.price}
-                  </strong>
+                  <strong className="ms-auto">{Breakfast.price3}</strong>
                 </div>
                 <div className="border-top mt-2 pt-2">
-                  <small>{Menu.Breakfast.FriedChips.ket}</small>
+                  <small>Rich Milk and Foam</small>
                 </div>
               </div>
               <div className="menu-block my-4">
                 <div className="d-flex">
-                  <h6>{Menu.Breakfast.Cereal.nama}</h6>
+                  <h6>{Breakfast.nama4}</h6>
                   <span className="underline" />
-                  <strong className="ms-auto">
-                    {Menu.Breakfast.Cereal.price}
-                  </strong>
+                  <strong className="ms-auto">{Breakfast.price4}</strong>
                 </div>
                 <div className="border-top mt-2 pt-2">
-                  <small>{Menu.Breakfast.Cereal.ket}</small>
+                  <small>Fresh brewed coffee and steamed milk</small>
                 </div>
               </div>
               <div className="menu-block">
                 <div className="d-flex">
-                  <h6>{Menu.Breakfast.BananaCake.nama}</h6>
+                  <h6>{Breakfast.nama5}</h6>
                   <span className="underline" />
-                  <strong className="ms-auto">
-                    {Menu.Breakfast.BananaCake.price}
-                  </strong>
+                  <strong className="ms-auto">{Breakfast.price5}</strong>
                 </div>
                 <div className="border-top mt-2 pt-2">
-                  <small>{Menu.Breakfast.BananaCake.ket}</small>
+                  <small>Rich Milk and Foam</small>
                 </div>
               </div>
             </div>
@@ -81,70 +101,62 @@ const Menu = () => {
             <div className="menu-block-wrap">
               <div className="text-center mb-4 pb-lg-2">
                 <em className="text-white">Favourite Menu</em>
-                <h4 className="text-white">{Menu.Coffee.nama}</h4>
+                <h4 className="text-white">Coffee</h4>
               </div>
               <div className="menu-block">
                 <div className="d-flex">
-                  <h6>{Menu.Coffee.Latte.nama}</h6>
+                  <h6>{Coffee.kopi1}</h6>
                   <span className="underline" />
                   <strong className="text-white ms-auto">
                     <del>$12.50</del>
                   </strong>
-                  <strong className="ms-2">{Menu.Coffee.Latte.price}</strong>
+                  <strong className="ms-2">{Coffee.harga1}</strong>
                 </div>
                 <div className="border-top mt-2 pt-2">
-                  <small>{Menu.Coffee.Latte.ket}</small>
+                  <small>Fresh brewed coffee and steamed milk</small>
                 </div>
               </div>
               <div className="menu-block my-4">
                 <div className="d-flex">
                   <h6>
-                    {Menu.Coffee.WhiteCoffee.nama}
+                    {Coffee.kopi2}
                     <span className="badge ms-3">Recommend</span>
                   </h6>
                   <span className="underline" />
-                  <strong className="ms-auto">
-                    {Menu.Coffee.WhiteCoffee.price}
-                  </strong>
+                  <strong className="ms-auto">{Coffee.harga2}</strong>
                 </div>
                 <div className="border-top mt-2 pt-2">
-                  <small>{Menu.Coffee.WhiteCoffee.ket}</small>
+                  <small>Brewed coffee and steamed milk</small>
                 </div>
               </div>
               <div className="menu-block">
                 <div className="d-flex">
-                  <h6>{Menu.Coffee.ChocolateMilk.nama}</h6>
+                  <h6>{Coffee.kopi3}</h6>
                   <span className="underline" />
-                  <strong className="ms-auto">
-                    {Menu.Coffee.ChocolateMilk.price}
-                  </strong>
+                  <strong className="ms-auto">{Coffee.harga3}</strong>
                 </div>
                 <div className="border-top mt-2 pt-2">
-                  <small>{Menu.Coffee.ChocolateMilk.ket}</small>
+                  <small>Rich Milk and Foam</small>
                 </div>
               </div>
               <div className="menu-block my-4">
                 <div className="d-flex">
-                  <h6>{Menu.Coffee.Greentea.nama}</h6>
+                  <h6>{Coffee.kopi4}</h6>
                   <span className="underline" />
-                  <strong className="ms-auto">
-                    {Menu.Coffee.Greentea.price}
-                  </strong>
+                  <strong className="ms-auto">{Coffee.harga4}</strong>
                 </div>
                 <div className="border-top mt-2 pt-2">
-                  <small>{Menu.Coffee.Greentea.ket}</small>
+                  <small>Fresh brewed coffee and steamed milk</small>
                 </div>
               </div>
               <div className="menu-block">
                 <div className="d-flex">
-                  <h6>{Menu.Coffee.DarkChocolate.nama}</h6>
+                  <h6>{Coffee.kopi5}</h6>
                   <span className="underline" />
-                  <strong className="ms-auto">
-                    {Menu.Coffee.DarkChocolate.price}
-                  </strong>
+                  <strong className="ms-auto">{Coffee.harga5}</strong>
                 </div>
                 <div className="border-top mt-2 pt-2">
-                  <small>{Menu.Coffee.DarkChocolate.ket}</small>
+                  <small>Rich Milk and Foam</small>
                 </div>
               </div>
             </div>
